c14c2ce4fec805d62e0a4f89a7d4d0982d96d6a5
lucas <lucas@DESKTOP-AG6IHSG>
2023-01-14 15:15:03 -0300

    adicionando algumas coisinhas

--- javatestando/src/curso/Pessoa.java
+++ javatestando/src/curso/Pessoa.java
@@ -2,9 +2,15 @@ package curso;
 
 public class Pessoa {
 
-		private String : nome;
-		as
-		asdasdasd
-		asdasd
+		private String nome;
+		
+		private String sobrenome;
+		
+		private int idade;
+		
+		private Date dataNascimento;
+		
+		private Date dataNascimento2;
+		
 		
 }
